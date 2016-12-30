b72c149f71eea0da25ef44d3950505a4cb6451fa
MichaelHBBrooks <Michael.H.B.Brooks@gmail.com>
2016-12-29 19:04:47 -0500

    Created a project that uses the shared library, mySharedLibrary.

---
+++ helloWorldDLLExe/src/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Dec 29, 2016
+ *      Author: jinieren
+ */
+
+#include "someClass.h"
+#include <iostream>
+
+int main(){
+	std::cout << "1" << std::endl;
+	someClass a;
+	//a.printText();
+	std::cout << "2" << std::endl;
+	return 0;
+}
