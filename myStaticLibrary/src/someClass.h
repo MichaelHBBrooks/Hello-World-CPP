a1cb9b5081ec06a859414997cb46401c277dc868
MichaelHBBrooks <Michael.H.B.Brooks@gmail.com>
2016-12-29 19:52:00 -0500

    Static library creation. This should theoretically work.

---
+++ myStaticLibrary/src/someClass.h
@@ -0,0 +1,18 @@
+/*
+ * someClass.h
+ *
+ *  Created on: Dec 29, 2016
+ *      Author: jinieren
+ */
+
+#ifndef SRC_SOMECLASS_H_
+#define SRC_SOMECLASS_H_
+
+class someClass {
+public:
+	someClass();
+
+	void printText();
+};
+
+#endif /* SRC_SOMECLASS_H_ */
